659a104d1114a627ad3fb43d3dba21943aed10be
vick <vkspidey007@gmail.com>
2024-09-03 21:27:55 +0530

    hu

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables
-  const env = loadEnv(mode, process.cwd(), '')
-  process.env = { ...process.env, ...env }
-
-  return {
-    base: './',
-    build: {
-      outDir: 'build',
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Ensure this matches the location of your HTML file
     },
-  }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Adjust if you use aliases
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
 })
