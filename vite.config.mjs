425ea72c0ab69d84837bfb6e6cc25ed828f59978
vick <vkspidey007@gmail.com>
2024-09-03 21:21:22 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig(({ mode }) => {
   // Load environment variables
@@ -12,47 +9,6 @@ export default defineConfig(({ mode }) => {
     base: './',
     build: {
       outDir: 'build',
-      rollupOptions: {
-        input: {
-          main: 'index.html', // Default entry point
-          // Additional entries if needed
-        },
-      },
-    },
-    css: {
-      postcss: {
-        plugins: [
-          autoprefixer({}), // Configure as needed
-        ],
-      },
-    },
-    define: {
-      'process.env': process.env,
-    },
-    esbuild: {
-      loader: 'jsx',
-      include: /src\/.*\.jsx?$/,
-    },
-    optimizeDeps: {
-      include: [
-        // Add specific dependencies to include
-      ],
-    },
-    plugins: [react()],
-    resolve: {
-      alias: [
-        {
-          find: 'src/',
-          replacement: `${path.resolve(__dirname, 'src')}/`,
-        },
-      ],
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
-    },
-    server: {
-      port: 3000,
-      proxy: {
-        // Configure proxy if needed
-      },
     },
   }
 })
